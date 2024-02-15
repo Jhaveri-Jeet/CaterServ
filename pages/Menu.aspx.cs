f69df5b665e5d2741dfaa43778d164117ba80ce9
darshitdudhaiya <darshitdudhaiya201@gmail.com>
2024-02-16 00:11:11 +0530

    Menu page is now dynamic

--- pages/Menu.aspx.cs
+++ pages/Menu.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,40 @@ namespace CaterServ
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                DataTable categories = Common.Services.select("SELECT * FROM [Category]");
+                if (categories.Rows.Count > 0)
+                {
+                    categoryRepeater.DataSource = categories;
+                    categoryRepeater.DataBind();
+                }
 
+                int id = Convert.ToInt32(categories.Rows[0]["Id"]);
+
+                DataTable menu = Common.Services.select("SELECT * FROM [Menu] WHERE CategoryId = " + id);
+                if (menu.Rows.Count > 0)
+                {
+                    menuRepeater.DataSource = menu;
+                    menuRepeater.DataBind();
+                }
+            }
         }
+
+        protected void categoryRepeater_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+            if (e.CommandName == "Select")
+            {
+                int id = Convert.ToInt32(e.CommandArgument);
+
+                DataTable menu = Common.Services.select("SELECT * FROM [Menu] WHERE CategoryId = " + id);
+                if (menu.Rows.Count > 0)
+                {
+                    menuRepeater.DataSource = menu;
+                    menuRepeater.DataBind();
+                }
+            }
+        }
+
     }
 }
